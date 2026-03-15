cc35e6f7a49d9fafc69b56e1477a2bcd50fa65b1
sc3n3moxd-alt <sc3n3moxd@gmail.com>
2026-03-16 06:30:48 +0800

    Update app.js

--- app.js
+++ app.js
@@ -84,6 +84,9 @@ function tick() {
       saveXP();
       updateXP();
       saveSession(); // <-- LOG SESSION
+      distractions = 0;
+      localStorage.setItem("distractions", distractions);
+      updateStats();
       assistantSay("Time to take a break~");
       catImg.src = "cat_sleep.gif";
       play("purr");
@@ -94,6 +97,7 @@ function tick() {
       assistantSay("Break finished~ Ready to focus again?");
       catImg.src = "cat_focus.gif";
       clearInterval(timer);
+      updateStats();
     }
   }
 }
